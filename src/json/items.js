2b42fdec548fe11ec92539255c40c1ebe8679371
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-01-23 23:56:07 +0600

    filter added

---
+++ src/json/items.js
@@ -0,0 +1,79 @@
+const item = [
+  {
+    id: 1,
+    title: "android",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "Project name",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "android",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "etc",
+    category: "etc",
+  },
+  {
+    id: 1,
+    title: "android",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "android",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "web",
+    category: "web",
+  },
+  {
+    id: 1,
+    title: "web",
+    category: "web",
+  },
+  {
+    id: 1,
+    title: "Project name",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "Project name",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "Project name",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "Project name",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "Project name",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "Project name",
+    category: "android",
+  },
+  {
+    id: 1,
+    title: "Project name",
+    category: "android",
+  },
+];
+
+export default item;
