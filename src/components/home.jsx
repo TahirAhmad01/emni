3680f5e47ab24a57b5f9abca49669eaca74d3a2a
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-01-23 23:55:51 +0600

    filter added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,61 +1,62 @@
-import React from "react";
-import Modal from "react-modal";
+import React, { useState } from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../css/style.css";
-
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
+import item from "../json/items";
 
 export default function Home() {
-  let subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [items, setfilter] = useState(item);
 
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
+  const handleClick = (name) => {
+    const updateFilter = item.filter((filtered) => {
+      if (name === "all") {
+        return item;
+      } else {
+        return filtered.category === name;
+      }
+    });
+    setfilter(updateFilter);
+  };
 
   return (
     <div>
       <h1>This is Home page</h1>
-      <button onClick={openModal}>Open Modal</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
-        <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>
-      </Modal>
+      <div className="d-flex justify-content-center">
+        <button
+          className="btn btn-primary mx-2"
+          onClick={handleClick.bind(this, "all")}
+        >
+          all
+        </button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={handleClick.bind(this, "android")}
+        >
+          android
+        </button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={handleClick.bind(this, "web")}
+        >
+          web
+        </button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={handleClick.bind(this, "etc")}
+        >
+          etc
+        </button>
+      </div>
 
-      <i className="fad fa-abacus"></i>
-      <i className="fab fa-accusoft"></i>
+      <div className="row px-3">
+        {items.map((item, idx) => (
+          <div className="col-4 px-3 my-4 border border-primary" key={idx}>
+            <div className={item.category === ""}>
+              <div>{item.title}</div>
+              <div>{item.category}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
